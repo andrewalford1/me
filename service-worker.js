7bc2f4c5e66f41390da5d00f1b078c9fdf4e21cf
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-05-24 13:55:48 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.f61e1a2773052e43d3cfec4323fa4dfb.js"
+  "/me/precache-manifest.c75a7c374ed55efb3a6be2b95767bfa5.js"
 );
 
 self.addEventListener('message', (event) => {
