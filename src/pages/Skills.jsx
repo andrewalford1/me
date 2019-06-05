fbf97e8eac322f8e6ec57de1df61ea02f8cb7c10
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2019-06-05 16:48:08 +0100

    Updated skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,10 +3,38 @@ import React from 'react';
 const Skills = () => (
 	<div>
 		<hr />
-		<h1 className='App-Header'>Skills - Coming Soon</h1>
-		<div>
-			I do have some, I just have not had the time to write this section
-			yet :(
+		<h1 className='App-Header'>Skills</h1>
+		<div style={{ marginTop: '2%', marginBottom: '2%' }}>
+			<div id={`Skills-languages`}>
+				<h2>Languages</h2>
+				<ul>
+					<li>JavaScript</li>
+					<li>C#</li>
+					<li>C++</li>
+					<li>Java</li>
+					<li>Python</li>
+				</ul>
+			</div>
+			<div id={`Skills-librariesAndAPIs`}>
+				<h2>Libraries & APIs</h2>
+				<ul>
+					<li>React</li>
+					<li>THREE JS</li>
+					<li>THREE JS</li>
+					<li>CANNON JS</li>
+					<li>OpenGL</li>
+					<li>DirectX</li>
+				</ul>
+			</div>
+			<div id={`Skill-softwareAndTools`}>
+				<h2>Software & Tools</h2>
+				<ul>
+					<li>VS Code & Visual Studio</li>
+					<li>Eclipse</li>
+					<li>Overleaf</li>
+					<li>The Adobe Suite</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 );
